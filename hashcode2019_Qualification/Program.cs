ebf7cfe2731f31495f845fe86feca6e42c4afdb0
Mohammed Farhan <mufarhan@earthlink.iq>
2019-02-28 17:50:17 +0300

    Initial Commit

---
+++ hashcode2019_Qualification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hashcode2019_Qualification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
